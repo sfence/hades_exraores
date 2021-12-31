6ec3350177fe6735c569feb6c1d0fda1c960a009
SFENCE <sfence.software@gmail.com>
2021-12-31 15:21:30 +0100

    Fix bug in platinum ore generation. Add levels with bigger ore concreation for some ores.

--- ores.lua
+++ ores.lua
@@ -32,7 +32,28 @@ minetest.register_ore({
   y_max          = -20000,
   flags          = "absheight",
  })
-
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "hades_extraores:stone_with_bauxite",
+  wherein        = "hades_core:stone",
+  clust_scarcity = 10*10*10,
+  clust_num_ores = 27,
+  clust_size     = 4,
+  y_min          = -22220,
+  y_max          = -22120,
+  flags          = "absheight",
+ })
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "hades_extraores:stone_with_bauxite",
+  wherein        = "hades_core:stone",
+  clust_scarcity = 8*8*8,
+  clust_num_ores = 27,
+  clust_size     = 4,
+  y_min          = -22175,
+  y_max          = -22165,
+  flags          = "absheight",
+ })
 
 -- nickel (-15k)
 minetest.register_ore({
@@ -54,6 +75,28 @@ minetest.register_ore({
  clust_size     = 3,
  y_min          = -31000,
  y_max          = -20000,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_garnierite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 11*11*11,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -26700,
+ y_max          = -26600,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_garnierite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 9*9*9,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -26655,
+ y_max          = -26645,
  flags          = "absheight",
 })
 
@@ -79,6 +122,28 @@ minetest.register_ore({
  y_max          = -25000,
  flags          = "absheight",
 })
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_rulite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 11*11*11,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -28400,
+ y_max          = -28300,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_rulite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 9*9*9,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -28355,
+ y_max          = -28345,
+ flags          = "absheight",
+})
 
 -- Tungsten (-25k)
 minetest.register_ore({
@@ -100,6 +165,28 @@ minetest.register_ore({
  clust_size     = 3,
  y_min          = -31000,
  y_max          = -30000,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_wolframite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 11*11*11,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -30200,
+ y_max          = -30100,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_wolframite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 9*9*9,
+ clust_num_ores = 5,
+ clust_size     = 3,
+ y_min          = -30155,
+ y_max          = -30145,
  flags          = "absheight",
 })
 
@@ -125,11 +212,33 @@ minetest.register_ore({
  y_max          = -30000,
  flags          = "absheight",
 })
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_spodumene",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 11*11*11,
+ clust_num_ores = 55,
+ clust_size     = 9,
+ y_min          = -30400,
+ y_max          = -30300,
+ flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:stone_with_spodumene",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 9*9*9,
+ clust_num_ores = 55,
+ clust_size     = 9,
+ y_min          = -30350,
+ y_max          = -30340,
+ flags          = "absheight",
+})
 
 -- Platinum
 minetest.register_ore({
  ore_type       = "scatter",
- ore            = "hades_extraores:stone_with_silver",
+ ore            = "hades_extraores:stone_with_platinum",
  wherein        = "hades_core:stone",
  clust_scarcity = 13*13*13,
  clust_num_ores = 1,
@@ -140,7 +249,7 @@ minetest.register_ore({
 
 minetest.register_ore({
  ore_type       = "scatter",
- ore            = "hades_extraores:stone_with_silver",
+ ore            = "hades_extraores:stone_with_platinum",
  wherein        = "hades_core:stone",
  clust_scarcity = 11*11*11,
  clust_num_ores = 5,
@@ -161,7 +270,6 @@ minetest.register_ore({
  y_min          = -31000,
  y_max          = 0,
 })
-
 minetest.register_ore({
  ore_type       = "scatter",
  ore            = "hades_extraores:halite",
@@ -172,6 +280,36 @@ minetest.register_ore({
  y_min          = -31000,
  y_max          = -2048,
  flags          = "absheight",
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:halite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 100*100*100,
+ clust_num_ores = 512,
+ clust_size     = 8,
+ y_min          = -9000,
+ y_max          = -7000,
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:halite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 40*40*40,
+ clust_num_ores = 48,
+ clust_size     = 6,
+ y_min          = -8500,
+ y_max          = -7500,
+})
+minetest.register_ore({
+ ore_type       = "scatter",
+ ore            = "hades_extraores:halite",
+ wherein        = "hades_core:stone",
+ clust_scarcity = 20*20*20,
+ clust_num_ores = 48,
+ clust_size     = 6,
+ y_min          = -8200,
+ y_max          = -7800,
 })
 
 -- silver
